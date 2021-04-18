9ad6a5086db6dd5893ec0f2cd44f46c5614af8de
Nancy Chan <nancychannc@gmail.com>
2021-04-17 18:04:16 -0700

    remove duplicate import, rearrange imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { environment } from '../environments/environment';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { reducers } from './state/app.state';
+
+import { CovidEffects } from './covid/state/covid.effects';
+import { DeathsEffects } from './deaths/state/deaths.effects';
+
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { reducers } from './state/app.state';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+
 import { CovidComponent } from './covid/covid.component';
 import { DeathsComponent } from './deaths/deaths.component';
 import { SearchComponent } from './search/search.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatSelectModule } from '@angular/material/select';
-import { FormsModule } from '@angular/forms';
-import { EffectsModule } from '@ngrx/effects';
-import { HttpClientModule } from '@angular/common/http';
-import { CovidEffects } from './covid/state/covid.effects';
-import { DeathsEffects } from './deaths/state/deaths.effects';
 
 @NgModule({
   declarations: [
@@ -31,23 +36,22 @@ import { DeathsEffects } from './deaths/state/deaths.effects';
   ],
   imports: [
     BrowserModule,
-    StoreModule.forRoot(reducers),
-    EffectsModule.forRoot([CovidEffects, DeathsEffects]),
     BrowserAnimationsModule,
     FormsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    StoreModule.forRoot(reducers),
+    EffectsModule.forRoot([CovidEffects, DeathsEffects]),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
     MatToolbarModule,
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
     MatFormFieldModule,
     MatSelectModule,
-    FlexLayoutModule,
-    HttpClientModule,
-    StoreDevtoolsModule.instrument({
-      maxAge: 25,
-      logOnly: environment.production,
-    }),
-    EffectsModule.forRoot([]),
   ],
   providers: [],
   bootstrap: [AppComponent],
