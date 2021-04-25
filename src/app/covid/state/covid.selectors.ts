d06e14ddd97e380a8bfb300027c33d49292f09d4
channancy <nancychannc@gmail.com>
2021-04-24 19:20:04 -0700

    format data in selectors

--- src/app/covid/state/covid.selectors.ts
+++ src/app/covid/state/covid.selectors.ts
@@ -2,4 +2,14 @@ import { createSelector } from '@ngrx/store';
 import { AppState } from 'src/app/state/app.state';
 
 export const select = (state: AppState) => state;
-export const Covid = createSelector(select, (state: AppState) => state.covid);
+export const Covid = createSelector(select, (state: AppState) =>
+  state.covid.stats
+    ? {
+        population: state.covid.stats.population,
+        cases: state.covid.stats.actuals.cases,
+        deaths: state.covid.stats.actuals.deaths,
+        vaccinesDistributed: state.covid.stats.actuals.vaccinesDistributed,
+        vaccinesAdministered: state.covid.stats.actuals.vaccinesAdministered,
+      }
+    : null
+);
--- src/app/deaths/state/deaths.selectors.ts
+++ src/app/deaths/state/deaths.selectors.ts
@@ -2,4 +2,18 @@ import { createSelector } from '@ngrx/store';
 import { AppState } from 'src/app/state/app.state';
 
 export const select = (state: AppState) => state;
-export const Deaths = createSelector(select, (state: AppState) => state.deaths);
+export const Deaths = createSelector(select, (state: AppState) =>
+  state.deaths.stats
+    ? state.deaths.stats.reduce((list, d) => {
+        list.push({
+          weekEndingDate: d.week_ending_date,
+          allCause: d.all_cause,
+          naturalCause: d.natural_cause,
+          diabetes: d.diabetes_mellitus_e10_e14,
+          influenzaAndPneumonia: d.influenza_and_pneumonia_j09_j18,
+          heartDisease: d.diseases_of_heart_i00_i09,
+        });
+        return list;
+      }, [])
+    : null
+);
